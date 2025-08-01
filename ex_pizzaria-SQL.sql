c328bfa0b5626c44252455ce457b84c9abdf1f11
LucasNiello <terminiello.lucas@gmail.com>
2025-08-01 16:40:17 -0300

    exemplos_aula

---
+++ ex_pizzaria-SQL.sql
@@ -0,0 +1,14 @@
+create database ex_pizzaria;
+use ex_pizzaria;
+
+create table funcionario (
+id_funcionario int primary  key
+);
+
+create table pizza (
+id_pizza int primary  key
+);
+
+create table cliente (
+id_cliente int primary  key
+);
